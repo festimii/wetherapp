c98bb0626fb5f53dcd2841dbee62ff9fc9b20121
Festim <festimi-rrezoni@hotmail.com>
2020-04-30 01:21:09 +0200

    Updates

---
+++ precache-manifest.7f3ceb3e6f13b8f3288380cf5ce4a1b0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54a75ecf97efea4bdcb74cf84bae3c04",
+    "url": "/festimii/wetherapp.git/index.html"
+  },
+  {
+    "revision": "d301c8270f00d5e6baeb",
+    "url": "/festimii/wetherapp.git/static/css/main.f4a4fdb8.chunk.css"
+  },
+  {
+    "revision": "d6d6c0d36847f7175d8e",
+    "url": "/festimii/wetherapp.git/static/js/2.d71bb4e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/festimii/wetherapp.git/static/js/2.d71bb4e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d301c8270f00d5e6baeb",
+    "url": "/festimii/wetherapp.git/static/js/main.1d804c18.chunk.js"
+  },
+  {
+    "revision": "500a0f188f46f1ffb7d0",
+    "url": "/festimii/wetherapp.git/static/js/runtime-main.f79fb667.js"
+  },
+  {
+    "revision": "1af06fc14684b7ff75f5b57718cba2d0",
+    "url": "/festimii/wetherapp.git/static/media/cold-bg-phone.1af06fc1.jpg"
+  },
+  {
+    "revision": "8a998baabcb51adf1b8df143bf42fe6b",
+    "url": "/festimii/wetherapp.git/static/media/cold-bg.8a998baa.jpg"
+  },
+  {
+    "revision": "635ed87d2cc826fb213057ac40dfaff1",
+    "url": "/festimii/wetherapp.git/static/media/warm-bg.635ed87d.jpg"
+  }
+]);
